4fe02acf9f9b664a299b9b296e8d1c84674e2eb3
Dodilanne <dodilanne@ada-anvil.io>
2024-06-12 14:10:29 -0400

    Add async wallet connection example

--- src/documentation/examples/b-simple-wallet-connect/app.tsx
+++ src/documentation/examples/b-simple-wallet-connect/app.tsx
@@ -4,13 +4,34 @@ import { SUPPORTED_WALLETS } from "@/lib/utils";
 import { useEffect } from "react";
 
 export const App = () => {
-  const { wallet, connectWallet } = useWalletContext();
+  const { wallet, connectWallet, connectWalletAsync } = useWalletContext();
 
   useEffect(() => {
     if (wallet.isConnected) {
       wallet.handler.initialize();
     }
   }, [wallet]);
+
+  // @ts-ignore:next-line
+  const handleConnectSync = (key: string) => {
+    connectWallet(key, {
+      onSuccess(wallet) {
+        console.log("wallet", wallet);
+      },
+      onError(error) {
+        console.log("error", error);
+      },
+    });
+  };
+
+  const handleConnectAsync = async (key: string) => {
+    try {
+      const wallet = await connectWalletAsync(key);
+      console.log("wallet", wallet);
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
 
   return (
     <ExampleContainer>
@@ -22,7 +43,7 @@ export const App = () => {
                 key={key}
                 type="button"
                 className="btn btn-primary"
-                onClick={() => connectWallet(key)}
+                onClick={() => handleConnectAsync(key)}
               >
                 {displayName}
               </button>
