a9cedad31481d8a85a6c39e532d5849df05fc996
Dodilanne <dodilanne@ada-anvil.io>
2024-06-14 14:11:21 -0400

    Cleanup api

--- src/lib/main/persistence.ts
+++ src/lib/main/persistence.ts
@@ -23,15 +23,18 @@ export function getPersistedValue(key: keyof typeof STORAGE_KEYS): string | unde
 
 export const weldLocalStorage: WeldStorage = {
   get(key) {
-    if (typeof window === "undefined") return undefined;
-    return window.localStorage.getItem(key) ?? undefined;
+    if (typeof window !== "undefined") {
+      return window.localStorage.getItem(key) ?? undefined;
+    }
   },
   set(key, value) {
-    if (typeof window === "undefined") return;
-    window.localStorage.setItem(key, value);
+    if (typeof window !== "undefined") {
+      window.localStorage.setItem(key, value);
+    }
   },
   remove(key) {
-    if (typeof window === "undefined") return;
-    window.localStorage.removeItem(key);
+    if (typeof window !== "undefined") {
+      window.localStorage.removeItem(key);
+    }
   },
 };
--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -1,4 +1,4 @@
 export * from "./errors";
 export * from "./extensions";
-export * from "./wallets";
 export * from "./lovelace";
+export * from "./wallets";
