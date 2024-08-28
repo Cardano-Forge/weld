179b01e9465ef64eab9f46b19ca3b8572bbe7b09
Dodilanne <dodilanne@ada-anvil.io>
2024-08-28 15:31:14 -0400

    Improve stake util type

--- src/lib/main/utils/strip-address-prefix.ts
+++ src/lib/main/utils/strip-address-prefix.ts
@@ -1,6 +1,11 @@
 import { isChangeAddressHex, isStakeAddressHex } from "./extensions";
 
-export function stripAddressPrefix(input: string): string {
+export function stripAddressPrefix(input: string): string;
+export function stripAddressPrefix(input?: string | null): string | undefined;
+export function stripAddressPrefix(input?: string | null): string | undefined {
+  if (typeof input !== "string") {
+    return undefined;
+  }
   if (isStakeAddressHex(input) || isChangeAddressHex(input)) {
     return input.slice(2);
   }
