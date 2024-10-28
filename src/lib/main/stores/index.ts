46b4b2dcceb5e98a8ebf859c83ad49189871feaa
Dodilanne <dodilanne@ada-anvil.io>
2024-10-28 18:26:32 -0400

    Create isolated weld instances

--- src/lib/main/stores/index.ts
+++ src/lib/main/stores/index.ts
@@ -6,50 +6,55 @@ import { type ConfigStore, type WeldConfig, createConfigStore } from "./config";
 import { type ExtensionsStore, createExtensionsStore } from "./extensions";
 import { type WalletStore, createWalletStore } from "./wallet";
 
-let configStore: ConfigStore;
-let walletStore: WalletStore;
-let extensionsStore: ExtensionsStore;
+export function createWeldInstance() {
+  let configStore: ConfigStore;
+  let walletStore: WalletStore;
+  let extensionsStore: ExtensionsStore;
+  return {
+    get config() {
+      if (!configStore) {
+        configStore = createConfigStore();
+      }
+      return configStore;
+    },
+    get wallet() {
+      if (!walletStore) {
+        walletStore = createWalletStore();
+      }
+      return walletStore;
+    },
+    get extensions() {
+      if (!extensionsStore) {
+        extensionsStore = createExtensionsStore();
+      }
+      return extensionsStore;
+    },
+    persist(config?: Partial<WeldConfig>) {
+      this.config.persist();
+      this.wallet.persist({ tryToReconnectTo: config?.wallet?.tryToReconnectTo });
+      this.extensions.persist();
+    },
+    init({ persist = true }: { persist?: boolean | Partial<WeldConfig> } = {}) {
+      initCustomWallets(this.config.getState());
 
-export const weld = {
-  get config() {
-    if (!configStore) {
-      configStore = createConfigStore();
-    }
-    return configStore;
-  },
-  get wallet() {
-    if (!walletStore) {
-      walletStore = createWalletStore();
-    }
-    return walletStore;
-  },
-  get extensions() {
-    if (!extensionsStore) {
-      extensionsStore = createExtensionsStore();
-    }
-    return extensionsStore;
-  },
-  persist(config?: Partial<WeldConfig>) {
-    this.config.persist();
-    this.wallet.persist({ tryToReconnectTo: config?.wallet?.tryToReconnectTo });
-    this.extensions.persist();
-  },
-  init({ persist = true }: { persist?: boolean | Partial<WeldConfig> } = {}) {
-    initCustomWallets(this.config.getState());
+      if (typeof persist === "object") {
+        this.persist(persist);
+      } else if (persist) {
+        this.persist();
+      }
 
-    if (typeof persist === "object") {
-      this.persist(persist);
-    } else if (persist) {
-      this.persist();
-    }
+      this.config.init();
+      this.wallet.init();
+      this.extensions.init();
+    },
+    cleanup() {
+      this.config.cleanup();
+      this.wallet.cleanup();
+      this.extensions.cleanup();
+    },
+  };
+}
 
-    this.config.init();
-    this.wallet.init();
-    this.extensions.init();
-  },
-  cleanup() {
-    this.config.cleanup();
-    this.wallet.cleanup();
-    this.extensions.cleanup();
-  },
-};
+export const weld = createWeldInstance();
+
+export type WeldInstance = typeof weld;
--- src/lib/poly/stores/index.ts
+++ src/lib/poly/stores/index.ts
@@ -7,56 +7,61 @@ import { type EvmWalletStore, createEvmWalletStore } from "@/internal/evm/wallet
 import { type ConfigStore, type WeldConfig, createConfigStore } from "@/lib/main/stores/config";
 import { POLY_EXTENSIONS } from "../types";
 
-let configStore: ConfigStore<EvmConfig>;
-let walletStore: EvmWalletStore;
-let extensionsStore: EvmExtensionsStore;
+export function createWeldPolyInstance() {
+  let configStore: ConfigStore<EvmConfig>;
+  let walletStore: EvmWalletStore;
+  let extensionsStore: EvmExtensionsStore;
+  return {
+    get config() {
+      if (!configStore) {
+        configStore = createConfigStore();
+      }
+      return configStore;
+    },
+    get wallet() {
+      if (!walletStore) {
+        walletStore = createEvmWalletStore({
+          chain: "poly",
+          extensions: this.extensions,
+          config: this.config,
+          storageKey: "connectedPolyWallet",
+        });
+      }
+      return walletStore;
+    },
+    get extensions() {
+      if (!extensionsStore) {
+        extensionsStore = createEvmExtensionsStore({
+          extensions: POLY_EXTENSIONS,
+          config: this.config,
+        });
+      }
+      return extensionsStore;
+    },
+    persist(config?: Partial<WeldConfig>) {
+      this.config.persist();
+      this.wallet.persist({ tryToReconnectTo: config?.wallet?.tryToReconnectTo });
+      this.extensions.persist();
+    },
+    init({ persist = true }: { persist?: boolean | Partial<WeldConfig> } = {}) {
+      if (typeof persist === "object") {
+        this.persist(persist);
+      } else if (persist) {
+        this.persist();
+      }
 
-export const weldPoly = {
-  get config() {
-    if (!configStore) {
-      configStore = createConfigStore();
-    }
-    return configStore;
-  },
-  get wallet() {
-    if (!walletStore) {
-      walletStore = createEvmWalletStore({
-        chain: "poly",
-        extensions: this.extensions,
-        config: this.config,
-        storageKey: "connectedPolyWallet",
-      });
-    }
-    return walletStore;
-  },
-  get extensions() {
-    if (!extensionsStore) {
-      extensionsStore = createEvmExtensionsStore({
-        extensions: POLY_EXTENSIONS,
-        config: this.config,
-      });
-    }
-    return extensionsStore;
-  },
-  persist(config?: Partial<WeldConfig>) {
-    this.config.persist();
-    this.wallet.persist({ tryToReconnectTo: config?.wallet?.tryToReconnectTo });
-    this.extensions.persist();
-  },
-  init({ persist = true }: { persist?: boolean | Partial<WeldConfig> } = {}) {
-    if (typeof persist === "object") {
-      this.persist(persist);
-    } else if (persist) {
-      this.persist();
-    }
+      this.config.init();
+      this.wallet.init();
+      this.extensions.init();
+    },
+    cleanup() {
+      this.config.cleanup();
+      this.wallet.cleanup();
+      this.extensions.cleanup();
+    },
+  };
+}
 
-    this.config.init();
-    this.wallet.init();
-    this.extensions.init();
-  },
-  cleanup() {
-    this.config.cleanup();
-    this.wallet.cleanup();
-    this.extensions.cleanup();
-  },
-};
+export const weldPoly = createWeldPolyInstance();
+
+export type WeldPolyInstance = typeof weldPoly;
