668069d46457a0b7748bcbe09b1d00a0af97f6ed
Dodilanne <dodilanne@ada-anvil.io>
2025-02-20 12:22:38 -0500

    Fix dev server and add example

--- documentation/examples/a-wallet-state/app.tsx
+++ documentation/examples/a-wallet-state/app.tsx
@@ -1,8 +1,16 @@
 import { ExampleContainer } from "@/documentation/commons/example-container";
+import { parseBalance } from "@/lib/main";
 import { useWallet } from "@/lib/react";
+import { useMemo } from "react";
 
 export const App = () => {
   const wallet = useWallet();
+
+  const fullBalance = useMemo(() => {
+    if (wallet.balanceCbor) {
+      return parseBalance(wallet.balanceCbor);
+    }
+  }, [wallet.balanceCbor]) 
 
   return (
     <ExampleContainer>
@@ -31,6 +39,14 @@ export const App = () => {
               <div>Lovelace:</div>
               <div className="py-2 px-4 text-xs bg-slate-100 rounded-md break-words">
                 {wallet.balanceAda.toFixed(2)}
+              </div>
+              <div>Encoded balance:</div>
+              <div className="py-2 px-4 text-xs bg-slate-100 rounded-md break-words">
+                {wallet.balanceCbor}
+              </div>
+              <div>Full balance:</div>
+              <div className="py-2 px-4 text-xs bg-slate-100 rounded-md break-words">
+                {JSON.stringify(fullBalance, null, 2)}
               </div>
             </>
           )}
