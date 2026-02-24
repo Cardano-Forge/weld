fef0bb1ddc5c8089c9bcc3cd7345b51d46beb089
Dodilanne <vincaudet@gmail.com>
2026-02-24 13:24:35 -0500

    WIP: Connect

--- plugins/hodei/src/dev.ts
+++ plugins/hodei/src/dev.ts
@@ -1,1 +1,44 @@
-console.log('hello dev');
+import { type WalletStoreState, weld } from "@ada-anvil/weld";
+import { builtinPlugins } from "@ada-anvil/weld/plugins";
+import { hodeiPlugin } from "./lib";
+
+weld.config.update({
+  debug: true,
+  plugins: [...builtinPlugins, hodeiPlugin],
+  onUpdateError(context, error) {
+    console.error(context, error);
+  },
+});
+
+weld.wallet.subscribe((wallet) => {
+  const statusEl = document.querySelector("#status");
+  if (statusEl) statusEl.textContent = getStatus(wallet);
+  const nameEl = document.querySelector("#name");
+  if (nameEl) nameEl.textContent = wallet.displayName ?? "-";
+  const balanceEl = document.querySelector("#balance");
+  if (balanceEl) balanceEl.textContent = (wallet.balanceAda ?? "-").toString();
+});
+
+document.querySelector("#connect")?.addEventListener("click", async () => {
+  weld.wallet.connect("hodei", {
+    onError: console.error,
+  });
+});
+
+document.querySelector("#disconnect")?.addEventListener("click", async () => {
+  weld.wallet.disconnect();
+});
+
+window.addEventListener("load", () => {
+  weld.init();
+});
+
+function getStatus(wallet: WalletStoreState): string {
+  if (wallet.isConnected) {
+    return "connected";
+  }
+  if (wallet.isConnecting) {
+    return "connecting";
+  }
+  return "disconnected";
+}
--- plugins/hodei/src/lib.ts
+++ plugins/hodei/src/lib.ts
@@ -1,1 +1,31 @@
-console.log("hello lib");
+import {
+  DefaultWalletHandler,
+  getDefaultWalletConnector,
+  runOnce,
+} from "@ada-anvil/weld/core";
+import type { WeldPlugin } from "@ada-anvil/weld/plugins";
+import { initialize } from "hodei-client";
+
+class HodeiHandler extends DefaultWalletHandler {
+  async disconnect(): Promise<void> {
+    if (
+      "disconnect" in this.enabledApi &&
+      typeof this.enabledApi.disconnect === "function"
+    ) {
+      return this.enabledApi?.disconnect();
+    }
+  }
+}
+
+export const hodeiPlugin: WeldPlugin = {
+  key: "hodei",
+  connector: getDefaultWalletConnector(HodeiHandler),
+  initialize: runOnce(() => {
+    const wallet = initialize({
+      onError: (error) => console.error("error", error),
+      onClose: (state) => console.log("closed", state),
+    });
+    console.log("wallet", wallet);
+    return !!wallet;
+  }),
+};
